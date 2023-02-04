36609254ce71fda4714468183b2166c528cec808
antoniovalentic <antoniomax98@gmail.com>
2023-02-05 00:00:44 +0100

    InventoryItems structure fixes

--- Assets/Scripts/InventoryItems.cs
+++ Assets/Scripts/InventoryItems.cs
@@ -1,31 +1,24 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class InventoryItems : MonoBehaviour
+public struct ItemDesc {
+    public ItemDesc(string title, string picture) {
+        this.title = title;
+        this.picture = picture;
+    }
+    public string title { get; set; }
+    public string picture { get; set; }
+}
+
+public enum Item {
+    Matches,
+    Torch
+}
+
+public static class InventoryItems
 {
-
-    public struct ItemDesc {
-        public ItemDesc(string title, string picture) {
-            this.title = title;
-            this.picture = picture;
-        }
-        public string title { get; set; }
-        public string picture { get; set; }
-    }
-
-    public enum Item {
-        Matches,
-        Torch
-    }
-
-    public Dictionary<Item, ItemDesc> ItemDictionary = new Dictionary<Item, ItemDesc>(){
+    public static Dictionary<Item, ItemDesc> ItemDictionary = new Dictionary<Item, ItemDesc>(){
         { Item.Matches, new ItemDesc("Matches", "test.png") },
         { Item.Torch, new ItemDesc("Torch", "test.png") }
     };
-
-    private void Start() {
-        var testTitle = ItemDictionary[Item.Torch].title;
-    }
 
 }

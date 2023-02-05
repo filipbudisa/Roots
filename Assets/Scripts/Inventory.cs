71766f4e6cf98e2a4ea57ea7cc3925e32ff23846
antoniovalentic <antoniomax98@gmail.com>
2023-02-05 13:47:45 +0100

    Added UI methods to Inventory class

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    private GameObject uiTopLayer;
+    private GameObject uiExpoDiary;
+
+    private CursorControl cursor;
     private HashSet<Item> Items;
     public bool HasItem(Item item) {
         return Items.Contains(item);
@@ -43,6 +47,15 @@ public class Inventory : MonoBehaviour
         UpdateInventoryUI();
     }
 
+    private void Awake() {
+        var master = GameObject.Find("Master");
+        cursor = master.GetComponent<CursorControl>();
+
+        uiTopLayer = GameObject.Find("TopLayer");
+        uiExpoDiary = GameObject.Find("ExpoDiary");
+        HideExpoDiary();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -57,5 +70,32 @@ public class Inventory : MonoBehaviour
 
     private void UpdateInventoryUI() {
         // TODO: create inventory UI
+    }
+
+    public void UiCursorHoverEnter() {
+        cursor.setHover(true);
+    }
+    public void UiCursorHoverExit() {
+        cursor.setHover(false);
+    }
+
+    public void UiDiaryBtnClick() {
+        if(uiTopLayer.activeSelf == true) {
+            HideTopLayer();
+            ShowExpoDiary();
+        }
+    }
+
+    private void HideTopLayer() {
+        uiTopLayer.SetActive(false);
+    }
+    private void ShowTopLayer() {
+        uiTopLayer.SetActive(true);
+    }
+    private void HideExpoDiary() {
+        uiExpoDiary.SetActive(false);
+    }
+    private void ShowExpoDiary() {
+        uiExpoDiary.SetActive(true);
     }
 }

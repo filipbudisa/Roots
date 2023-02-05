a9511cd014d96e23e7365255e6670be840db2951
subo <gamedevsubo@gmail.com>
2023-02-05 12:30:26 +0100

    Add SceneSwitcher and InterActDoor

---
+++ Assets/Scripts/InteractActionDoor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractActionDoor : InteractAction
+{
+    SceneSwitcher sceneSwitcher;
+    [SerializeField] private string nextScene;
+    protected override void Action()
+    {
+        sceneSwitcher.ChangeScene(nextScene);
+    }
+    // Start is called before the first frame update
+    private void Awake() {
+        var master = GameObject.Find("Master");
+        sceneSwitcher = master.GetComponent<SceneSwitcher>();
+    }
+
+    private void OnMouseDown() {
+        Action();
+    }
+}
